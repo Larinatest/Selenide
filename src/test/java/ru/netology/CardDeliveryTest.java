8ada3fd889dd208b3d20e96b0a79d98505428e98
Evgenia Larina <larsik29@yandex.ru>
2024-10-12 13:08:26 +0300

    Add .idea/ to .gitignore and remove from index

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,2 +1,45 @@
-package ru.netology;public class СardDeliveryTest {
+package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+
+    private String formatDate(int days, String pattern) {
+        LocalDate date = LocalDate.now().plusDays(days);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return date.format(formatter);
+    }
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void deliveryCardForm() throws InterruptedException {
+        String planing = formatDate(4, "dd.MM.yyyy");
+        SelenideElement form = $(".form");
+
+        form.$("[data-test-id='city'] .input__control").setValue("Москва");
+        form.$("[data-test-id='date'] .input__control").sendKeys(Keys.CONTROL + "a"); //выделяет текст в поле
+        form.$("[data-test-id='date'] .input__control").sendKeys(Keys.DELETE); // удаляет выделеный текст
+        form.$("[data-test-id='date'] .input__control").setValue(planing);
+        form.$("[data-test-id='name'] .input__control").setValue("Иван Иванов");
+        form.$("[data-test-id='phone'] .input__control").setValue("+79880000000");
+        form.$("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification'] .notification__content").shouldHave(text("Встреча успешно забронирована на " + planing));
+    }
+
 }
